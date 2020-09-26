89f11b85c20bce8e98b10168dfe1b849b8aae0b7
Jprakash2002 <jpsvrk123@gmail.com>
2020-09-26 21:29:37 +0530

    dkfk

---
+++ foo.py
@@ -0,0 +1,29 @@
+from random import *
+from time import *
+while True:
+         print('LET SEE HOW YOUR BRAIN PREDICTS THE NUMBER GIVEN BY ME')
+         j=sleep(2)
+         print("WAIT A MOMENT, FIRST I WILL IMAGINE A NUMBER FROM 1 TO 10:)")
+         a=sleep(6)
+         print("OH YES! TELL ME THE NUMBER ")
+         a=sleep(1)
+         x=int(input())
+         y=randint(0,10)
+         m=sleep(3)
+         if x==y:
+             print("WOW! YOU GUESSED CORRECTLY :)  YOU ARE TOO SMART")
+             c=sleep(2)
+             print('LETS TRY AGAIN')
+             z=sleep(1)
+             print('-----------------------------------------------------------')
+             g=sleep(1)
+
+         else:
+             print("OH NO! YOU GUESSED WRONGLY :(  ")
+             d=sleep(2)
+             print("THE CORRECT ANSWER IS   ",y)
+             e=sleep(4)
+             print("DON'T WORRY ,,, I WILL GIVE YOU ANOTHER CHANCE ")
+             f=sleep(2)
+             print('------------------------------------------------------------')
+             h=sleep(1)
